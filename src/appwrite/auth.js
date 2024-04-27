221603f1fb53651826c11e63a9790e2a15dc8c2b
Aadesh2953 <shuklaaadesh0@gmail.com>
2024-04-27 10:36:00 +0530

    Added Backend

---
+++ src/appwrite/auth.js
@@ -0,0 +1,57 @@
+import config from "../conf/config.js";
+import { Client, Account, ID } from "appwrite";
+export class AuthService {
+  client = new Client();
+  account = new Account();
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteurl)
+      .setProject(config.appwriteProjectId);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      let userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        //    return userAccount
+        return this.login(userAccount);
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      console.log(err);
+    }
+  }
+  async login({ email, password }) {
+    try {
+      let credentials = this.account.createEmailSession(email, password);
+      return credentials;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log(error);
+    }
+    return null;
+  }
+  async logout()
+  {
+    try {
+        await this.account.deleteSessions()
+    } catch (error) {
+        console.log("error")
+    }
+  }
+
+}
+const authservice = new AuthService();
+export default authservice;
+// here we can copy paste the code from the same website but we dont because we need to export everything from the auth.js file so it will be difficult amd messy for
---
+++ src/conf/config.js
@@ -0,0 +1,9 @@
+const config={
+    appwriteurl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+export default config
+// this is needed because sometimes env variables making the whole application to crash 
