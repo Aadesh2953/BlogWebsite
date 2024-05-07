28c0c29458b5b3f7a0a750f4ebe2ca3be6ad6491
Aadesh2953 <shuklaaadesh0@gmail.com>
2024-05-07 09:49:57 +0530

    Added Router

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
-import { createBrowserRouter, createRoutesFromElements,Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,Route, RouterProvider } from 'react-router-dom'
 import {AuthLayout, Home,Login} from './components/index.js'
 const router=createBrowserRouter([
   {
